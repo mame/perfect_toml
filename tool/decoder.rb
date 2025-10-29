c96b87b2122003f8c0635195d46e4ec11a90042b
Yusuke Endoh <mame@ruby-lang.org>
2025-10-30 00:46:21 +0900

    Add support for TOML 1.1.0

--- tool/decoder.rb
+++ tool/decoder.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/env ruby
 
+# usage: TOML_DECODER_VERSION=1.0 /path/to/toml-test-v2.1.0-linux-amd64 test -toml 1.0 -decoder tool/decoder.rb
+# usage: TOML_DECODER_VERSION=1.1 /path/to/toml-test-v2.1.0-linux-amd64 test -toml 1.1 -decoder tool/decoder.rb
+
 require_relative "../lib/perfect_toml"
 require "json"
 
-def convert(toml)
+def toml_to_json(toml)
   case toml
-  when Hash then toml.to_h {|k, v| [k, convert(v)] }
-  when Array then toml.map {|v| convert(v) }
+  when Hash then toml.to_h {|k, v| [k, toml_to_json(v)] }
+  when Array then toml.map {|v| toml_to_json(v) }
   when String then { "type" => "string", "value" => toml }
   when Integer then { "type" => "integer", "value" => toml.to_s }
   when Float
@@ -32,4 +35,7 @@ def convert(toml)
   end
 end
 
-puts JSON.generate(convert(PerfectTOML.parse($stdin.read.force_encoding("UTF-8"))))
+puts JSON.generate(toml_to_json(PerfectTOML.parse(
+  $stdin.read.force_encoding("UTF-8"),
+  version: ENV.fetch("TOML_DECODER_VERSION", "1.0.0")
+)))
--- tool/encoder.rb
+++ tool/encoder.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 
+# usage: TOML_DECODER_VERSION=1.0 /path/to/toml-test-v2.1.0-linux-amd64 test -toml 1.0 -decoder tool/decoder.rb -encoder tool/encoder.rb
+# usage: TOML_DECODER_VERSION=1.1 /path/to/toml-test-v2.1.0-linux-amd64 test -toml 1.1 -decoder tool/decoder.rb -encoder tool/encoder.rb
+
 require_relative "../lib/perfect_toml"
 require "json"
 require "time"
 
-def convert(json)
-  return json.map {|v| convert(v) } if Array === json
+def json_to_toml(json)
+  return json.map {|v| json_to_toml(v) } if Array === json
   if json.key?("type")
     type, val = json["type"], json["value"]
     case type
@@ -31,17 +34,17 @@ def convert(json)
       raise if val !~ /\A(\d{2}):(\d{2}):(\d{2}(?:\.\d+)?)\z/
       PerfectTOML::LocalTime.new($1, $2, $3)
     else
-      json.to_h {|k, v| [k, convert(v)] }
+      json.to_h {|k, v| [k, json_to_toml(v)] }
     end
   else
-    json.to_h {|k, v| [k, convert(v)] }
+    json.to_h {|k, v| [k, json_to_toml(v)] }
   end
 end
 
-opts = {
+puts PerfectTOML.generate(
+  json_to_toml(JSON.parse($stdin.read.force_encoding("UTF-8"))),
   use_dot: ENV["TOML_ENCODER_USE_DOT"] == "1",
   sort_keys: ENV["TOML_ENCODER_SORT_KEYS"] == "1",
   use_literal_string: ENV["TOML_ENCODER_USE_LITERAL_STRING"] == "1",
   use_multiline_string: ENV["TOML_ENCODER_USE_MULTILINE_STRING"] == "1",
-}
-puts PerfectTOML.generate(convert(JSON.parse($stdin.read.force_encoding("UTF-8"))), **opts)
+)
